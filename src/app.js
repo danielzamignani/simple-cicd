3ea9542bafdb17e0ceb33af70af9945b42f252ef
Daniel Zamignani <danielzamignani@gmail.com>
2024-05-29 17:35:13 -0300

    refact: change in html

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ const express = require( "express" );
 const app = express();
 
 app.get( '/', ( req, res ) => {
-    res.send( '<h1>Hello World!!!<h1>' );
+    res.send( `
+        <h1>Hello World!!!<h1>
+        <h2>CodeDeploy test!</h2>
+    ` );
 } );
 
 app.listen( 3000, () => {
